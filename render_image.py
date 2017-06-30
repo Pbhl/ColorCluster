8bcd989dc9a1afb9e399274dd658861fb89464b3
Pbhl <parthb102@googlemail.com>
2017-07-01 04:15:58 +0530

    Added test csv and script to render image from csv files

---
+++ render_image.py
@@ -0,0 +1,9 @@
+from PIL import Image, ImageDraw
+import pandas as pd
+m=2 #magnification
+df=pd.read_csv("billgates.csv")#read csv file
+out = Image.new("RGB",(int((df['X'].max()*m)),int((df['Y'].max()*m)))) #create new blank image of appropriate size
+dout = ImageDraw.Draw(out)
+for index,row in df.iterrows():
+    dout.point((int(row['X'])*m,int(row['Y'])*m),fill=(int(row['R']),int(row['G']),int(row['B']))) #fill in points by iterating over csv
+out.show() #output image
