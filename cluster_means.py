f923ac039a8fb0df759716425994d6b0adcb75ed
Pbhl <parthb102@googlemail.com>
2017-07-02 03:17:02 +0530

    optimised performance of cluster_means.py

--- cluster_means.py
+++ cluster_means.py
@@ -1,14 +1,9 @@
-import pandas as pd
 def calc_mean():
+    import pandas as pd
     df = pd.read_csv("cluster_raw.csv")#read cluster info file
     df = df[['X','Y','R','G','B','Assignments']]#remove any redundant columns
-    df_clust = df.groupby(['Assignments'])['R','G','B'].mean()#calculate rgb means for every cluster
+    df_clust = df.groupby(['Assignments'])['R','G','B'].mean().reset_index().set_index('Assignments').T.to_dict('list')#calculate rgb means for every cluster
     #replace color values with mean values of respective cluster
-    for x in range(31 + 1):
-        for index,row in df.iterrows():
-            if (row['Assignments'] == x):
-                row['R']=df_clust.loc[x]['R']
-                row['G']=df_clust.loc[x]['G']
-                row['B']=df_clust.loc[x]['B']
-    df = df[['X','Y','R','G','B']]
+    df[['R','G','B']]=pd.DataFrame(x for x in df['Assignments'].map(df_clust))
+    df.drop('Assignments', axis=1, inplace=True)
     df.to_csv("output_img.csv")
--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import convert_to_csv
 import k_means
 import cluster_means
 import render_image
-ip_path = "billgates.png"
-op_path = "billgates_cluster.png"
+ip_path = "WIN_20170402_17_26_41_Pro.jpg"
+op_path = "output_img.jpg"
 convert_to_csv.img_to_csv(ip_path)
 k_means.cluster()
 cluster_means.calc_mean()
